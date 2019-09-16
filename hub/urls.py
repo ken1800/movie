4d57bcdfb859db66e9315dbdf20b745f904e4c6e
kenneth irungu <kennethirungu1800@gmail.com>
2019-09-16 03:21:47 -0400

    added static file templates

--- hub/urls.py
+++ hub/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path,include
+from .views import hubList,hubDetail
 
 urlpatterns = [
   
+    #path('kenny',views.index, name='index'),
+     path('', hubList.as_view(), name='hublist_view'),
+     path('<int:pk>/', hubDetail.as_view(), name='hubdetails-detail'),
+    #path('',)
     
 ]
--- hub/views.py
+++ hub/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from .models import hub
 
 # Create your views here.
+
+
+class hubDetail(DetailView):
+    model = hub
+    template_name = "index.html"
+    #context_object_name = 'manze umeweza kwa details view'
+    
+class hubList(ListView):
+    model = hub
+    #context_object_name = ''
+    template_name='index.html'
