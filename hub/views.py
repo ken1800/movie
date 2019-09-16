5678e801f84c886cb838137e0e7ef42122d53dcd
kenneth irungu <kennethirungu1800@gmail.com>
2019-09-16 06:17:35 -0400

    added pagination to the list view

--- hub/views.py
+++ hub/views.py
@@ -8,10 +8,12 @@ from .models import hub
 
 class hubDetail(DetailView):
     model = hub
-    template_name = "index.html"
+    template_name = "detail.html"
+    paginate_by =1
     #context_object_name = 'manze umeweza kwa details view'
     
 class hubList(ListView):
     model = hub
-    #context_object_name = ''
-    template_name='index.html'
+    context_object_name = 'list'
+    template_name='list.html'
+    paginate_by =2
